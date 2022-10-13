9e0a64cf959a3f97bc750667320bf475b5e70bb9
thascript <89532022+thascript@users.noreply.github.com>
2022-10-12 21:34:04 -0300

    finalizando

--- q3/Logar.java
+++ q3/Logar.java
@@ -1,4 +1,14 @@
 package q3;
 public class Logar {
-  
+
+  public Logar(int idLogin) {
+    opcoesLogin opl = new opcoesLogin();
+    Login[] lgs = opl.getLogins();
+
+    for (Login login: lgs){
+      if (login.getId() == idLogin){
+        System.out.println("Logado com Sucesso no: " + login.getTipo());
+      }
+    }  
+  } 
 }
