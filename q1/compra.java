93a96f8d0206e66a6c5b6ea150940dd90a96f28a
Roberto <33937381+jrobertogram@users.noreply.github.com>
2022-10-13 01:32:06 -0300

    att q1

--- q1/compra.java
+++ q1/compra.java
@@ -1,11 +1,18 @@
 package q1;
 
+import java.util.ArrayList;
+
+
 public class compra {
   public static void main(String[] args) {
 
+    ArrayList<Produto> contas = new ArrayList<>();
     Produto p1 = new Produto("bolo", 70);
     Produto p2 = new Produto("arroz", 5);
     Produto p3 = new Produto("feijao", 50);
+    contas.add(p1);
+    contas.add(p2);
+    contas.add(p3);
 
     carrinhoo car = new carrinhoo();
 
@@ -16,8 +23,7 @@ public class compra {
     produtos[2] = p3;
 		
 		car.setProdutos(produtos);
-    car.getPreco_total();
-    System.out.println( car.getPreco_total());
+    System.out.println(car.getPreco_total());
     car.lista_Produtos();
 
 
